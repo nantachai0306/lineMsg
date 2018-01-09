7bb859f11bdf930cd75e29f8dcee7cf3109c1af4
tknl45 <tknl45@users.noreply.github.com>
2018-01-09 11:38:24 +0800

    Update receive.php
    upload

--- receive.php
+++ receive.php
@@ -1,3 +1,8 @@
 <?php
-echo "Hi, I am 45."
+	$json_str = file_get_contents('php://input');
+	$json_obj = json_decode($json_str);
+	$myfile = fopen("log.txt", "w+") or die("unable open file");
+
+	fwrite($myfile, "\xEF\xBB\xBF".$json_str);
+	fclose($myfile);
 ?>
